8ceab90a6d8baa36adc040175110f099269515fe
meta <ankitm511@gmail.com>
2015-11-09 09:06:53 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/ankitm511/ridzapp/SaraBottomFragment.java
@@ -0,0 +1,18 @@
+package com.example.ankitm511.ridzapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class SaraBottomFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.sara_bottom_fragment, container, false);
+        return view;
+    }
+}
